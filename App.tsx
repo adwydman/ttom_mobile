14079d8b5a82a79c78002d97f197382c52bad751
Adrian Wydmanski <awydmanski@cribl.io>
2023-01-30 16:26:08 -0600

    wip

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import * as Notifications from 'expo-notifications';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen, { HomeHeader } from './src/screens/HomeScreen';
+import StoryInfoScreen from './src/screens/StoryInfoScreen';
+import ConfirmPurchaseScreen from './src/screens/ConfirmPurchaseScreen';
+import StoryHomeScreen from './src/screens/story/StoryHomeScreen';
+import StoryTextMessagesScreen from './src/screens/story/StoryTextMessagesScreen';
+import StoryConversationScreen from './src/screens/story/StoryConversationScreen';
+import { rootStore } from './src/stores';
 
-export default function App() {
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function requestPermissionsAsync() {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  });
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  useEffect(() => {
+    requestPermissionsAsync()
+  }, [])
+
+  const [fontsLoaded] = useFonts({
+    Niveau_smallCaps: require('./src/assets/fonts/Niveau_smallCaps.ttf'),
+    NiveauGroteskMedium: require('./src/assets/fonts/NiveauGroteskMedium.ttf'),
+    NiveauGroteskRegular: require('./src/assets/fonts/NiveauGroteskRegular.ttf'),
+    NiveauGroteskLight: require('./src/assets/fonts/NiveauGroteskLight.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={rootStore}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ header: () => <HomeHeader /> }}/>
+          <Stack.Screen name="StoryInfo" component={StoryInfoScreen} />
+          <Stack.Screen name="ConfirmPurchase" component={ConfirmPurchaseScreen} options={{ headerBackTitle: 'Back', headerTitle: ''}} />
+          <Stack.Screen name="StoryHome" component={StoryHomeScreen} options={{ header: () => null }} />
+          <Stack.Screen name="StoryMessages" component={StoryTextMessagesScreen} options={{ headerTitle: 'Messages' }} />
+          <Stack.Screen name="StoryConversation" component={StoryConversationScreen} options={({ route }) => ({ title: route.params.screenTitle })} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+{/* <Button
+title="Press to schedule a notification"
+onPress={async () => {
+  await schedulePushNotification();
+}}
+/> */}
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            components: './src/components',
+            shared: './src/shared',
+            screens: './src/screens',
+            assets: './src/assets',
+            utils: './src/utils',
+            stores: './src/stores'
+          },
+        },
+      ],
+    ],
   };
 };
+''
