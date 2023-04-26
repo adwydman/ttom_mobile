faaf6e0f80c8612a4029daabed7b5d2294f93f78
Adrian Wydmanski <awydmanski@cribl.io>
2023-04-25 19:32:03 -0500

    Some optimization

--- src/screens/story/StoryConversationScreen.tsx
+++ src/screens/story/StoryConversationScreen.tsx
@@ -82,7 +82,7 @@ export default function StoryConversationScreen({ navigation, route }: IScreenPr
   const currentStory = useSelector((state: any) => state.storeSlice.currentStory);
   const conversation = textMessages[screenTitle];
 
-  const optimisticallyUpdateMessages = async () => {
+  const optimisticallyUpdateMessages = useCallback(async () => {
     const newRawMessages = rawMessages.map((message) => {
       let parsedMessage = message;
       if (seenMessages.has(message._id)) {
@@ -94,8 +94,6 @@ export default function StoryConversationScreen({ navigation, route }: IScreenPr
     })
 
     dispatch(setRawMessages(newRawMessages));
-
-    // test failure
 
     sendRequest('/userStoryTextMessages', {
       method: 'PUT',
@@ -112,9 +110,7 @@ export default function StoryConversationScreen({ navigation, route }: IScreenPr
     .catch(() => {
       dispatch(setRawMessages(rawMessages));
     })
-  }
-
-  let lastType: string|null = null;
+  }, [dispatch, rawMessages, seenMessages, userToken, currentStory]);
 
   useEffect(() => {
     navigation.setOptions({
@@ -138,113 +134,113 @@ export default function StoryConversationScreen({ navigation, route }: IScreenPr
     };
   }, [navigation, screenTitle]);
 
-  const unseenMessages = [];
-  const parsedMessages = [];
-  // let firstUnreadMessage = false;
-  let firstUnreadMessage2 = null;
-
-  for (let i = 0; i < conversation.length; i++) {
-    const { _id, message, whoFrom, enabledAt, seenByUser } = conversation[i];
-    const type = isMainCharacter(whoFrom) ? 'right' : 'left';
-
-    if (!seenMessages.has(_id)) {
-      unseenMessages.push(conversation[i]);
+  const scrollOnLoad = useCallback(() => {
+    if (firstUnreadMessage.current === false && scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({ x: 0, y: 999999, animated: false})
     }
+  }, [scrollViewRef, firstUnreadMessage]);
 
-    let shouldShowTail = true;
-
-    if (i < conversation.length - 1) {
-      const nextMessageType = isMainCharacter(conversation[i + 1].whoFrom) ? 'right' : 'left';
-      if (nextMessageType === type) {
-        shouldShowTail = false;
+  const parsedMessages = useMemo(() => {
+    const parsedMessages = [];
+    let firstUnreadMessage2 = null;
+    let lastType: string|null = null;
+  
+    for (let i = 0; i < conversation.length; i++) {
+      const { _id, message, whoFrom, enabledAt, seenByUser } = conversation[i];
+      const type = isMainCharacter(whoFrom) ? 'right' : 'left';
+      let shouldShowTail = true;
+  
+      if (i < conversation.length - 1) {
+        const nextMessageType = isMainCharacter(conversation[i + 1].whoFrom) ? 'right' : 'left';
+        if (nextMessageType === type) {
+          shouldShowTail = false;
+        }
       }
-    }
-
-    let extraStyles = {};
-    if (lastType !== null && type !== lastType) {
-      extraStyles = {
-        marginTop: 8
+  
+      let extraStyles = {};
+      if (lastType !== null && type !== lastType) {
+        extraStyles = {
+          marginTop: 8
+        }
       }
-    }
-
-    const wrapperStyle = {};
-    if (i + 1 === conversation.length) {
-      wrapperStyle.marginBottom = 40;
-    }
-
-    lastType = type;
-
-    const onChange = (isVisible: boolean) => {
-      setSeenMessages(new Set([...Array.from(seenMessages), conversation[i]._id])) 
-    }
-
-    const fullTimestamp = getMessageTimestamp(enabledAt);
-    const shouldDisplayCenteredTimestamp = getShouldDisplayCenteredTimestamp(i, conversation);
-
-    if (firstUnreadMessage2 === null && seenByUser === false) {
-      firstUnreadMessage2 = message;
-
+  
+      const wrapperStyle = {};
+      if (i + 1 === conversation.length) {
+        wrapperStyle.marginBottom = 40;
+      }
+  
+      lastType = type;
+  
+      const onChange = (isVisible: boolean) => {
+        setSeenMessages(new Set([...Array.from(seenMessages), _id])) 
+      }
+  
+      const fullTimestamp = getMessageTimestamp(enabledAt);
+      const shouldDisplayCenteredTimestamp = getShouldDisplayCenteredTimestamp(i, conversation);
+  
+      if (firstUnreadMessage2 === null && seenByUser === false) {
+        firstUnreadMessage2 = message;
+  
+        parsedMessages.push(
+          <View key={'unread'} style={{ position: 'relative', marginTop: 16, marginBottom: 16, alignItems: 'center' }}>
+            <View style={{ position: 'absolute', top: 6, height: 2, width: '100%', backgroundColor: 'black', opacity: 0.1}}></View>
+            <Text style={{ fontSize: 12, backgroundColor: '#f2f2f2', paddingLeft: 8, paddingRight: 8 }}>Unread</Text>
+          </View>
+        );
+      }
+  
+      if (shouldDisplayCenteredTimestamp) {
+        parsedMessages.push(
+          <View key={`${message}-${enabledAt}`} style={{ marginTop: 12, marginBottom: 12, alignItems: 'center' }}>
+            <Text style={{ fontSize: 12 }}>
+              {fullTimestamp}
+            </Text>
+          </View>
+        )
+      }
+  
       parsedMessages.push(
-        <View key={'unread'} style={{ position: 'relative', marginTop: 16, marginBottom: 16, alignItems: 'center' }}>
-          <View style={{ position: 'absolute', top: 6, height: 2, width: '100%', backgroundColor: 'black', opacity: 0.1}}></View>
-          <Text style={{ fontSize: 12, backgroundColor: '#f2f2f2', paddingLeft: 8, paddingRight: 8 }}>Unread</Text>
-        </View>
+        <InView
+          key={i}
+          onChange={onChange}
+          style={wrapperStyle}
+          onLayout={(event) => {
+            if (message === firstUnreadMessage2) {
+              if (scrollViewRef.current) {
+                firstUnreadMessage.current = true;
+  
+                let offset = 40;
+                if (shouldDisplayCenteredTimestamp) {
+                  offset = 80;
+                }
+  
+                scrollViewRef.current.scrollTo({ x: 0, y: event.nativeEvent.layout.y - offset, animated: false})
+              }
+            }
+          }}
+        >
+          <Message
+            type={type}
+            extraStyles={extraStyles}
+            shouldShowTail={shouldShowTail}
+            timestamp={fullTimestamp}
+            conversation={conversation[i]}
+            previousMessage={conversation[i - 1]}
+          >
+            {message}
+          </Message>
+        </InView>
       );
     }
 
-    if (shouldDisplayCenteredTimestamp) {
-      parsedMessages.push(
-        <View key={`${message}-${enabledAt}`} style={{ marginTop: 12, marginBottom: 12, alignItems: 'center' }}>
-          <Text style={{ fontSize: 12 }}>
-            {fullTimestamp}
-          </Text>
-        </View>
-      )
-    }
-
-    parsedMessages.push(
-      <InView
-        key={i}
-        onChange={onChange}
-        style={wrapperStyle}
-        onLayout={(event) => {
-          if (message === firstUnreadMessage2) {
-            if (scrollViewRef.current) {
-              firstUnreadMessage.current = true;
-
-              let offset = 40;
-              if (shouldDisplayCenteredTimestamp) {
-                offset = 80;
-              }
-
-              scrollViewRef.current.scrollTo({ x: 0, y: event.nativeEvent.layout.y - offset, animated: false})
-            }
-          }
-        }}
-      >
-        <Message
-          type={type}
-          extraStyles={extraStyles}
-          shouldShowTail={shouldShowTail}
-          timestamp={fullTimestamp}
-          conversation={conversation[i]}
-          previousMessage={conversation[i - 1]}
-        >
-          {message}
-        </Message>
-      </InView>
-    );
-  }
+    return parsedMessages;
+  }, [conversation, seenMessages])
 
   return (
     <StoryFrame navigation={navigation} onBothPress={optimisticallyUpdateMessages} route={route}>
       <IOScrollView
         ref={scrollViewRef}
-        onLayout={() => {
-          if (firstUnreadMessage.current === false && scrollViewRef.current) {
-            scrollViewRef.current.scrollTo({ x: 0, y: 999999, animated: false})
-          }
-        }}
+        onLayout={scrollOnLoad}
       >
         { parsedMessages }
       </IOScrollView>
